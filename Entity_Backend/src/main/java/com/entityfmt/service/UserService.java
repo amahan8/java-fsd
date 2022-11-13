20c4406003c51280301387dba5a7ca610af9684c
amahan8 <115523994+amahan8@users.noreply.github.com>
2022-11-13 19:51:28 +0530

    Add files via upload

---
+++ Entity_Backend/src/main/java/com/entityfmt/service/UserService.java
@@ -0,0 +1,51 @@
+package com.entityfmt.service;
+
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.entityfmt.entity.Users;
+import com.entityfmt.exception.OrderManagmentException;
+import com.entityfmt.payload.request.RegistrationPayload;
+import com.entityfmt.repository.UserRepository;
+
+@Service
+public class UserService {
+	
+	private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+	
+	@Autowired
+	UserRepository userRepo;
+
+	public Users getUser(String email) {
+		Optional<Users> users = userRepo.findByEmail(email);
+		return users.isPresent()?users.get():null;
+	}
+	
+	public boolean getUserExists(long id) {
+		return userRepo.findById(id).isPresent();
+	}
+
+	public Users updateUser(long userId, RegistrationPayload request) {
+		Optional<Users> userOptional = userRepo.findById(userId);
+			if(userOptional.isPresent()) {
+				Users user = userOptional.get();
+				if(!user.getEmail().equals(request.getEmail().trim())) {
+					if (userRepo.findByEmail(request.getEmail()).isPresent()) {
+					    throw new OrderManagmentException("Email already taken!!");
+					}
+					user.setEmail(request.getEmail());
+				}
+				
+				user.setFirstName(request.getFirstName());
+				user.setLastName(request.getLastName());
+				user.setPassword(request.getPassword());
+				return userRepo.save(user);
+			} else {
+				throw new OrderManagmentException("Email not found!!");
+			}
+	}
+}
